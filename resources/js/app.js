8076b1091ff3b868cc604946404121f987594ad8
sivacchi <hiroki.kh@gmail.com>
2020-06-09 12:31:21 +0900

    Google Analyticsの実装

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,18 +1,25 @@
 // require('./bootstrap');
 import get_location from "./get_location";
+import ga from "./ga"
+
+ga();
 
 const btn = document.querySelector('.js-submit-location');
 const btn2 = document.querySelector('.js-copy');
 
-btn.addEventListener('click', async (event)=> {
-    event.preventDefault();
-    const loc = await get_location();
-    document.querySelector('input[name="lat"]').value = loc.lat;
-    document.querySelector('input[name="lng"]').value = loc.lng;
-    document.forms.submit_location.submit();
-});
+if (btn !== null) {
+    btn.addEventListener('click', async (event) => {
+        event.preventDefault();
+        const loc = await get_location();
+        document.querySelector('input[name="lat"]').value = loc.lat;
+        document.querySelector('input[name="lng"]').value = loc.lng;
+        document.forms.submit_location.submit();
+    });
+}
 
-btn2.addEventListener('click', (event) =>{
-    event.preventDefault();
-    navigator.clipboard.writeText(event.currentTarget.dataset.title);
-});
+if (btn2 !== null) {
+    btn2.addEventListener('click', (event) => {
+        event.preventDefault();
+        navigator.clipboard.writeText(event.currentTarget.dataset.title);
+    });
+}
