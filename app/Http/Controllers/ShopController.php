a5526a2d7b2dee5e3a798f881d294c593fdc6b3b
sivacchi <hiroki.kh@gmail.com>
2020-09-03 21:55:52 +0900

    距離の調整

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -65,8 +65,8 @@ class ShopController extends Controller
                 $distance ."
                  as distance
                 ", [$lat, $lng, $lat])
-            ->whereRaw('shop_url_d_delivery is not null  or shop_url_rakuten_delivery is not null and ' .
-                $distance . '< 4',  [$lat, $lng, $lat])
+            ->whereRaw('(shop_url_d_delivery is not null  or shop_url_rakuten_delivery is not null) and ' .
+                $distance . '< 1.85',  [$lat, $lng, $lat])
             ->orderByRaw('shop_score desc ,' . $distance, [$lat, $lng, $lat])
             ->paginate(10);
 //        dd(compact('shops'));
