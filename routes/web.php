7114fc0c0740bdb53bbf603de5586dc5a63795fe
sivacchi <hiroki.kh@gmail.com>
2020-12-25 14:33:40 +0900

    IEMECI-123 町名がオーバーフローするので分割

--- routes/web.php
+++ routes/web.php
@@ -30,8 +30,15 @@ Route::group(['middleware' => 'auth.very_basic'], function()
   Route::get('sitemap.xml', 'SitemapController@index')->name('sitemap');
   Route::group(['prefix' => 'sitemaps'], function() {
     // sitemapのルート
-    Route::get('area_towns.xml', 'SitemapController@area_towns')->name('sitemap-area_towns');
+    Route::get('area_prefs.xml', 'SitemapController@area_prefs')->name('sitemap-area_prefs');
+    Route::get('towns_{area_group_id}.xml', 'SitemapController@towns')->name('sitemap-towns');
+    Route::get('streets_hokkaido.xml', 'SitemapController@streets_hokkaido')->name('sitemap-hokkaido');
+    Route::get('streets_tohoku.xml', 'SitemapController@streets_tohoku')->name('sitemap-tohoku');
     Route::get('streets_kanto.xml', 'SitemapController@streets_kanto')->name('sitemap-kanto');
+    Route::get('streets_chubu.xml', 'SitemapController@streets_chubu')->name('sitemap-chubu');
+    Route::get('streets_kinki.xml', 'SitemapController@streets_kinki')->name('sitemap-kinki');
+    Route::get('streets_chugoku.xml', 'SitemapController@streets_chugoku')->name('sitemap-chugoku');
+    Route::get('streets_kyushu.xml', 'SitemapController@streets_kyushu')->name('sitemap-kyushu');
     // sitemapを増やす場合はココに追記していく。
   });
 });
